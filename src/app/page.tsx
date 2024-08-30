1427a1f0b2b52ba9cd9c9fed81c9c024b576928c
shoei03 <fujin.raijin.1415@gmail.com>
2024-08-30 14:51:03 +0900

    add pictures on Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-
+    <div className="relative w-[500px] h-[300px]">
+      <Image
+        src="/backGround-0.png"
+        layout="fill"
+        objectFit="cover"
+        alt="background"
+        className="absolute"
+      />
+      <Image
+        src="/backGround-1.png" // 重ねる画像のパスを指定
+        layout="fill"
+        objectFit="contain"
+        alt="overlay"
+        className="absolute"
+      />
     </div>
   );
 }
