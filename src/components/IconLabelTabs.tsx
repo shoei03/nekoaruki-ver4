018f77b40a30730149fe6293ab2be1a7ea28e8d3
shoei03 <fujin.raijin.1415@gmail.com>
2024-08-30 17:20:00 +0900

    divide COLLECTION and SETTING

--- src/components/IconLabelTabs.tsx
+++ src/components/IconLabelTabs.tsx
@@ -3,9 +3,10 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import DialpadIcon from '@mui/icons-material/Dialpad';
 import CollectionsIcon from '@mui/icons-material/Collections';
-import NotificationsIcon from '@mui/icons-material/Notifications';
+import SettingsIcon from '@mui/icons-material/Settings';
+import CollectionComponent from '../components/CollectionComponent';
+import SettingComponent from '../components/SettingComponent';
 
 export default function IconLabelTabs() {
   const [value, setValue] = React.useState(0);
@@ -15,13 +16,14 @@ export default function IconLabelTabs() {
   };
 
   return (
-    <div>
+    <div className="fixed top-0 w-full z-50">
       <Tabs value={value} onChange={handleChange} aria-label="icon label tabs example">
-        <Tab icon={<DialpadIcon />} label="GOAL" />
-        <Tab icon={<CollectionsIcon />} label="COLLECTIONS" />
-        <Tab icon={<NotificationsIcon />} label="NOTIFICATION" />
+        <Tab icon={<CollectionsIcon />} label="Collections" />
+        <Tab icon={<SettingsIcon />} label="Settings" />
       </Tabs>
       <div className="mt-4">
+        {value === 0 && <CollectionComponent />}
+        {value === 1 && <SettingComponent />}
       </div>
     </div>
   );
